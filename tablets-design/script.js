752b3d4478952fc3f9b9956398d3df0a22db39a1
Davidson Daniel <shylock160@gmail.com>
2024-11-05 23:10:15 -0800

    deleted the tablets-desgin branch

--- tablets-design/script.js
+++
@@ -1,9 +0,0 @@
-function myfunction(){
-    var showhide = document.getElementById('nav-menu');
-    if (showhide.style.display === "flex") {
-        showhide.style.display = "none";
-    }
-    else{
-        showhide.style.display = "flex";
-    }
-}
